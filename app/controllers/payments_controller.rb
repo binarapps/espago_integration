e7f92fd1fa62148a3dc6d8b0943f8f387c4da8d5
Michał Brodecki <m.brodecki@binarapps.com>
2014-07-23 15:28:25 +0200

    Charging client

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,5 +4,16 @@ class PaymentsController < ApplicationController
   end
 
   def charge_customer
+    if params[:card_token]
+      request = Espago.clients(:post, card: params[:card_token], description: "New client" ).body
+      puts "Creating client"
+      puts request
+      charge = {amount: 100.0, description: "Description", currency: 'pln', client: request["id"]}
+      puts "Charging client"
+      response = Espago.charges(:post, charge)      
+      puts response.body
+    else
+      redirect_to root_url
+    end
   end
 end
---
+++ config/initializers/espago.rb
@@ -0,0 +1,4 @@
+Espago.app_id = Rails.application.secrets.espago_app_id
+Espago.app_password = Rails.application.secrets.espago_app_password
+Espago.public_key = Rails.application.secrets.espago_public_key
+Espago.production = Rails.application.secrets.espago_production
